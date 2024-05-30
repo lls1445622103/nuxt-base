a7aaebf6543643839693d35e0a29ea7ac6a39ff6
lls <1445622103@qq.com>
2024-05-30 12:07:38 +0800

    下载依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: lls 1445622103@qq.com
+ * @Date: 2024-05-28 12:04:20
+ * @LastEditors: lls 1445622103@qq.com
+ * @LastEditTime: 2024-05-28 12:26:22
+ * @FilePath: /nuxt-base/nuxt.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
@@ -5,7 +13,8 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/ui',
     '@nuxt/fonts',
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    '@nuxtjs/google-fonts'
   ],
   ui: {
     icons: ['heroicons', 'simple-icons'],
