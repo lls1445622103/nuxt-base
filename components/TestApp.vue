0319a6d76926446a6fa0b5c12f4313b8c1369524
lls <1445622103@qq.com>
2024-07-12 17:36:13 +0800

    nuxt 设置新的模板

---
+++ components/TestApp.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: lls 1445622103@qq.com
+ * @Date: 2024-07-12 15:09:08
+ * @LastEditors: lls 1445622103@qq.com
+ * @LastEditTime: 2024-07-12 16:51:53
+ * @FilePath: /nuxt-base/components/test.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div @click="handle">{{ teet }}</div>
+</template>
+
+<script setup>
+const teet = defineModel();
+const app = ref(222);
+function handle() {
+  teet.value = "asdni";
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ layouts/element.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: lls 1445622103@qq.com
+ * @Date: 2024-07-12 17:03:04
+ * @LastEditors: lls 1445622103@qq.com
+ * @LastEditTime: 2024-07-12 17:03:11
+ * @FilePath: /nuxt-base/layouts/element.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+    element
+    <slot />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
